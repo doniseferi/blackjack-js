76b52a250a0cfa10423ae8d9b9a19d0a1455d233
Edonis Seferi <edonis.seferi@asos.com>
2018-08-22 20:38:24 +0100

    refactored the game state logic into its own class

--- src/scripts/blackJack.js
+++ src/scripts/blackJack.js
@@ -9,6 +9,7 @@ class BlackJack {
         let distributor = this.initalizeDistributor();
         let calculateHitStrategies = this.initalizeHitStrategies();
         this.dealer = new Dealer(this.pointsCalculator, this.players, deck, distributor, calculateHitStrategies);
+        this.blackJackGameState = new BlackJackGameState();
     };
 
     getCardsForPlayer(player) {
@@ -46,41 +47,11 @@ class BlackJack {
     }
 
     get state() {
-        let gameOver = this.players.some(x => x.hit) ? "Game Being Played" : "Game Over";
 
-        let winners = this.players.filter(x => !x.out); //.forEach(x => win.set(this.playerName(x), x.score));
-        let losers = this.players.filter(x => !winners.includes(x)); //.forEach(x => lose.set(this.playerName(x), x.score));
-
-        let win = new Map();
-        let lose = new Map();
-
-        winners.forEach(x => win.set(this.playerName(x), x.score));
-        losers.forEach(x => lose.set(this.playerName(x), x.score));
-
-        let isDealerWinner = win.has("Dealer");
-
-        let winRes = "";
-        let loseRes = "";
-
-        if (isDealerWinner) {
-            winRes += "The dealer is the winner";
-        } else {
-            winners.forEach(x => winRes += this.playerName(x) + " is a winner ")
-        }
-
-        losers.forEach(x => loseRes += this.playerName(x) + " is a loser ");
-
-        var partialResult = winRes + loseRes;
-
-        return gameOver == "Game Over" ?
-            gameOver + " " + partialResult :
-            gameOver + partialResult;
+        return this.blackJackGameState.state(this);
     }
 
-    playerName(player) {
-        let playerNo = this.players.indexOf(player) + 1;
-        return player.constructor.name == "Player" ? "Player" + playerNo : "Dealer";
-    }
+
 
     initalizeDeck(numberOfPlayers) {
         let deckFactory = new DeckFactory();
@@ -108,4 +79,44 @@ class BlackJack {
         return factory.create();
     }
 
+}
+
+class BlackJackGameState {
+    state(blackJack) {
+
+        let gameOver = blackJack.players.some(x => x.hit) ? "Game Being Played" : "Game Over";
+
+        let winners = blackJack.players.filter(x => !x.out); //.forEach(x => win.set(this.playerName(x), x.score));
+        let losers = blackJack.players.filter(x => !winners.includes(x)); //.forEach(x => lose.set(this.playerName(x), x.score));
+
+        let win = new Map();
+        let lose = new Map();
+
+        winners.forEach(x => win.set(this.playerName(x, blackJack.players), x.score));
+        losers.forEach(x => lose.set(this.playerName(x, blackJack.players), x.score));
+
+        let isDealerWinner = win.has("Dealer");
+
+        let winRes = "";
+        let loseRes = "";
+
+        if (isDealerWinner) {
+            winRes += "The dealer is the winner";
+        } else {
+            winners.forEach(x => winRes += this.playerName(x, blackJack.players) + " is a winner ")
+        }
+
+        losers.forEach(x => loseRes += this.playerName(x, blackJack.players) + " is a loser ");
+
+        var partialResult = winRes + loseRes;
+
+        return gameOver == "Game Over" ?
+            gameOver + " " + partialResult :
+            gameOver + partialResult;
+    }
+
+    playerName(player, players) {
+        let playerNo = players.indexOf(player) + 1;
+        return player.constructor.name == "Player" ? "Player" + playerNo : "Dealer";
+    }
 }
